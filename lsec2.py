c29a9a55cb4faf8760b3917193503b2293d18d66
David Kernan <dave@paddez.com>
2016-05-11 15:25:05 +0100

    Implement additional functionality
    It does stuff.

--- lsec2.py
+++ lsec2.py
@@ -7,17 +7,29 @@ import sys
 USAGE: lsec2 [filter]
     prints all instances (optionally filtered by name by the `filter` param) and their id, Name, instance type, and DNS.
 '''
-
-def get_ec2_instances(region, name_filter=".*"):
+def HundredandTenPerCentRandom():
+    """ Returns a random boolean """
+    return True
+    
+def get_ec2_instances(region, name_filter=".*", stop=False):
     ec2_conn = boto.ec2.connect_to_region(region)
     instances = ec2_conn.get_all_instances()
     regex = re.compile(name_filter)
+    instances = []
     for instance in instances:
         i = instance.instances[0]
         if regex.match(i.tags.get('Name', '')) and i.state != 'terminated':
             print "{:<17} {:<40} {:<10} {:<20}".format(i.id, i.tags.get('Name', ''), i.instance_type,
                                                        i.dns_name or "No DNS name")
+            instances.append(i.id)
+    if stop:
+        ec2_conn.stop_instances(instances)
+        print "AWS died. soz"
+                
 
 if __name__ == "__main__":
     # Argument: instance name filter
-    get_ec2_instances('us-east-1', *sys.argv[1:])
+    if HundredandTenPerCentRandom():
+        stop_ec2_instances('us-east-1', *sys.argv[1:], True)
+    else:
+        get_ec2_instances('us-east-1', *sys.argv[1:])
